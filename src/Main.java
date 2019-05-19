ea74614d5c56c99dd288c145f01831831f34a45a
manny <manny@ego>
2019-05-19 17:16:27 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,24 @@
  */
 
 public class Main {
-	public static void main(String[] args) {
+	public static boolean isPrime(long n) {
+		if (n <= 1)
+			return false;
+		if (n > 2 && (n & 1) == 0)
+			return false;
+		for (long i = 3; i <= Math.sqrt(n); i += 2)
+			if (n % i == 0)
+				return false;
+		return true;
+	}
 
+	public static void main(String[] args) {
+		long n = 0, p = 0;
+		do {
+			n += 1;
+			if (Main.isPrime(n))
+				p += 1;
+		} while (p < 10001);
+		System.out.format("ANSWER = %d\n", n);
 	}
 }
